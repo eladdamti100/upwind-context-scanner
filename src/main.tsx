3255e96b8f40e54c71652a49ffe835fb5673a74a
edanziv <edan.ziv@upwind.io>
2026-06-22 23:59:07 +0300

    Add Upwind design tokens and dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './theme/index.css';
 
 // SignalLens ships dark-theme only (matches the Upwind console design).
 document.documentElement.dataset.theme = 'dark';
